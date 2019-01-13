4ef816d6a38a139cc3a1d92ccf64be4c7b88c570
Jordan <jordanfievetpro@outlook.fr>
2019-01-13 18:56:21 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const div = document.querySelector('.container');
+
+
+
+
 class Personnage {
     constructor (nom , sante , force) {
         this.nom = nom;
@@ -8,12 +13,42 @@ class Personnage {
     decrire() {
         return `${this.nom} a ${this.sante} point de vie , ${this.force} force et ${this.xp} xp`
     }
+    attaquer(cible) {
+
+       
+
+        if (this.sante > 0) {
+    
+    
+          div.innerHTML += 
+    
+            `${this.nom} attaque ${cible.nom} et lui inflige ${this.force} points de dégâts <br />`
+    
+          ;
+    
+          cible.sante -= this.force;
 }
+
+        if(cible.sante > 0) {
+            this.sante -= cible.force
+                div.innerHTML += `${cible.nom} a encore ${cible.sante} points de vie <br />`;
+                div.innerHTML += `${cible.nom} attaque ${this.nom} et lui inflige ${cible.force} points de dégats <br /> `
+                div.innerHTML += `${this.nom} a encore ${this.sante} point de vie <br />`
+        }
+        
+    }};
 
 
 const aurora = new Personnage('Aurora', 100 , 8);
 const glacius = new Personnage('Glacius', 100, 15);
+const monstre = new Personnage('Archer squelette', 45, 3);
 
-const div = document.querySelector('.container');
-div.innerHTML = aurora.decrire();
+
+
+if(monstre.sante < 100 || aurora.sante < 100) {
+    while(monstre.sante > 0 ) {
+    monstre.attaquer(aurora)
+    }
+    
+}
 
